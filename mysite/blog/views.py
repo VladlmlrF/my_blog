79ec211fa9cfe81837936edd5d2971462d5abb75
VladlmlrF <vladimirfalin84@gmail.com>
2023-08-28 16:07:18 +0700

    create pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если номер страницы не целое число, то выдаст первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы находится вне диапазона, то выдаст последнюю имеющуюся страницу
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404('No Post found')
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
 
