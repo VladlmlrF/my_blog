36eca8c4aef8b00dacf747729e59d149489a7b78
VladlmlrF <vladimirfalin84@gmail.com>
2023-08-29 17:10:36 +0700

    create tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,29 +5,34 @@ from .forms import EmailPostForm, CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     # Постраничная разбивка с 3 постами на страницу
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         # Если номер страницы не целое число, то выдаст первую страницу
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # Если номер страницы находится вне диапазона, то выдаст последнюю имеющуюся страницу
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если номер страницы не целое число, то выдаст первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы находится вне диапазона, то выдаст последнюю имеющуюся страницу
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
