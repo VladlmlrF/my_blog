9bcd46a94657688c59d5c9958760cfe9d3c3a212
VladlmlrF <vladimirfalin84@gmail.com>
2023-08-31 13:57:39 +0700

    replaced sqlite with postgresql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'vova',
+        'PASSWORD': '3r1b4p3419',
+        'HOST': 'localhost',
+        'PORT': '5438',
     }
 }
 
